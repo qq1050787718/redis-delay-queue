468d074bd4e4c3f968c0a0f735dc3d6348b0300c
bobobo <hongbo.xiang@hand-china.com>
2022-05-18 18:41:38 +0800

    feature(delay-queue-core): container implements Lifecycle

--- delay-queue-core/src/main/java/org/areodynamic/redis/container/RedisDelayQueueContainer.java
+++ delay-queue-core/src/main/java/org/areodynamic/redis/container/RedisDelayQueueContainer.java
@@ -1,13 +1,15 @@
 package org.areodynamic.redis.container;
 
 import org.areodynamic.redis.Handler.TopicHandler;
+import org.springframework.context.Lifecycle;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RedisDelayQueueContainer {
+public class RedisDelayQueueContainer implements Lifecycle {
 
+    private volatile boolean running = false;
     private RedisTemplate redisTemplate;
     private final List<TopicHandler> handlers;
 
@@ -17,5 +19,24 @@ public class RedisDelayQueueContainer {
 
     public void setRedisTemplate(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void start() {
+        setRunning(true);
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public boolean isRunning() {
+        return this.running;
+    }
+
+    protected void setRunning(boolean running) {
+        this.running = running;
     }
 }
